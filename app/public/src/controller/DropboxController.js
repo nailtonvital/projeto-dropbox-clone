b39f0b06f0cbca6efb0a9a2b4754706d44e292f6
Nailton Vital <nailtonvital35@gmail.com>
2022-07-20 15:08:51 -0300

    update

---
+++ app/public/src/controller/DropboxController.js
@@ -0,0 +1,50 @@
+class DropboxController {
+    constructor(){
+
+        this.btnSendFileEl = document.querySelector('#btn-send-file')
+        this.inputFilesEl = document.querySelector('#files')
+        this.snackModalEl = document.querySelector('#react-snackbar-root')
+        this.initEvents();
+    }
+
+    initEvents(){
+        this.btnSendFileEl.addEventListener('click', e =>{
+            this.inputFilesEl.click()
+            
+        })
+
+        this.inputFilesEl.addEventListener('change', e =>{
+            this.uploadTask(e.target.files)
+            this.snackModalEl.style.display = 'block'
+        })
+    }
+
+    uploadTask(files){
+        let promises = [];
+        [...files].forEach(file=> {
+            promises.push(new Promise((resolve,reject)=>{
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload')
+                ajax.onload =event =>{
+                    try{
+                        resolve(JSON.parse(ajax.responseText))
+                    } catch(e){
+                        reject(e)
+                    }
+                }
+
+                ajax.onerror =event =>{
+                    reject(event)
+                }
+
+                let formData = new FormData();
+
+                formData.append('input-file', file)
+
+                ajax.send(formData)
+            }))
+        });
+        return Promise.all(promises)
+    }
+}
